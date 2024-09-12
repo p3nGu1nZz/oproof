c2e05272034a7a581d56930ea3e7240f26def8d4
p3n <rawsonkara@gmail.com>
2024-09-12 00:10:14 -0400

    provide context as well

--- oproof/template.py
+++ oproof/template.py
@@ -15,19 +15,20 @@ class Template:
     
     INSTRUCTIONS = (
         "You are an expert {{ system_type }} system.\n"
-        "Your task is to identify the domain and context of the given pair of prompt and response strings.\n"
-        "Return the domain and context as plain text.\n"
+        "Your task is to identify the domain, context, and is_valid of the given pair of prompt and response strings.\n"
+        "Return the domain, context, is_valid as plain text.\n"
         "Do not provide any explanations, markdown, code, or other content beside a JSON Object.\n"
         "Only return the domain and context of input prompt and response pair.\n"
-        "Return JSON Object of type { \"domain\": domain, \"context\": context }\n"
-        "The domains to choose from are: {{ domains }}."
-        "Infer the context based from input prompt and response pair in extrapolated domains"
+        "Return JSON Object of type { \"domain\": domain, \"context\": context, \"is_valid\" }\n"
+        "The domain to choose from are: {{ domains }}.\n"
+        "The context is extrapolated from input prompt and response pair based in selected domains.\n"
+        ""
     )
 
     PROMPT = (
         "System: {{ system }}\n"
         "Instructions: {{ instructions }}\n"
-        "Example: 'What is 2 + 2?' '4' returns 'basic math' with context 'arithmetic'\n"
+        "Example: {{ example }}\n"
         "User: {{ prompt }}\n"
         "Response: {{ response }}\n"
         "System: Return only the domain and context. No explanations, only the domain and context; e.g., {\"domain\": \"basic math\", \"context\": \"arithmetic\"}\n"
@@ -38,5 +39,5 @@ class Template:
     }
 
     TASKS = {
-        "proofs": "Proof the given prompt and response pair of input text strings. e.g., 'What is 2 + 2?' '4' returns 'basic math' with context 'arithmetic'\n"
+        "proofs": "Given prompt 'What is 2 + 2?' and response '4' returns 'basic math' with context 'arithmetic'"
     }
